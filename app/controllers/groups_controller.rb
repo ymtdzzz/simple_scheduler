ffc250e8e032e3fcb1693b110225d6ebdc0367a9
Y.Matsuda <zeroclock@icloud.com>
2019-01-31 23:04:53 +0000

    グループ編集＋作成者でないと編集／削除できないようにした

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :owner, only: [:edit, :update, :destroy]
+
   def index
     @groups = Group.paginate(page: params[:page])
   end
@@ -26,9 +28,17 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update_attributes(group_params)
+      flash[:success] = "グループ情報を更新しました"
+      redirect_to @group
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -38,5 +48,11 @@ class GroupsController < ApplicationController
 
     def group_params
       params.require(:group).permit(:name)
+    end
+
+    def owner
+      @group = Group.find(params[:id])
+      flash[:danger] = "グループの編集／削除は作成者のみが行えます"
+      redirect_to @group unless @group.owner_id == current_user.id        
     end
 end
